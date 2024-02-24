3d2405083c8cc1fe8f7c846e3116d4ec98e765d3
Nathan <natannvw@gmail.com>
2024-02-24 10:53:41 +0200

    finish visualize

--- tfrecord_utils.py
+++ tfrecord_utils.py
@@ -7,7 +7,10 @@ import pandas as pd
 
 def read_image(path):
     with rasterio.open(path) as src:
-        return src.read().astype(np.uint16)
+        image_data = src.read().astype(np.uint16)
+        image_data = np.transpose(image_data, (1, 2, 0))  # (height, width, channels)
+
+        return image_data
 
 
 def read_label(path):
@@ -64,47 +67,93 @@ def create_tf_example(
     return example_proto.SerializeToString()
 
 
-def write_tfrecord(tfrecord_filename, split_df, S2Hand_path, LabelHand_path):
+def write_tfrecord(tfrecord_filename, split_df, images_path, labels_path):
     """Writes given images and labels to a TFRecord file."""
     with tf.io.TFRecordWriter(tfrecord_filename) as writer:
         for idx, row in split_df.iterrows():
             image_path = os.path.join(
-                S2Hand_path, row["scene"].replace("S1Hand", "S2Hand")
+                images_path, row["scene"].replace("S1Hand", "S2Hand")
             )
-            label_path = os.path.join(LabelHand_path, row["mask"])
+            label_path = os.path.join(labels_path, row["mask"])
 
             image_array = read_image(image_path)
             label_array = read_label(label_path)
             chip_id = int(os.path.basename(image_path).split("_")[1])
-
-            # from matplotlib import pyplot as plt
-
-            # plt.figure()
-            # plt.imshow(label_array, cmap="turbo")
 
             example = create_tf_example(image_array, label_array, chip_id)
 
             writer.write(example)
 
 
+def get_feature_description() -> dict:
+    return {
+        "image": tf.io.FixedLenFeature([], tf.string),
+        "label": tf.io.FixedLenFeature([], tf.string),
+    }
+
+
+def _parse_function(example_proto, feature_description):
+    return tf.io.parse_single_example(example_proto, feature_description)
+
+
+def decode_image(image):
+    image = tf.io.decode_raw(image, tf.uint16)
+    image = tf.reshape(image, [512, 512, 13])  # Reshape to original shape
+    image = image.numpy()
+    image = np.transpose(
+        image, (2, 0, 1)
+    )  # Transpose back to (channels, height, width)
+
+    return image
+
+
+def decode_label(label):
+    label = tf.io.decode_raw(label, tf.uint8)
+    label = tf.reshape(label, [512, 512])  # Reshape to original shape
+    label = tf.cast(label, tf.int16)  # Cast to int16 to handle negative values
+    label = label.numpy()
+    label[label == 255] = -1  # convert 255 back to -1
+
+    return label
+
+
 if __name__ == "__main__":
+    from matplotlib import pyplot as plt
+
     S2Hand_path = "sen1floods11/v1.1/data/flood_events/HandLabeled/S2Hand/"
     LabelHand_path = "sen1floods11/v1.1/data/flood_events/HandLabeled/LabelHand/"
     splits_path = "sen1floods11/v1.1/splits/flood_handlabeled/"
 
     bolivia_data_path = os.path.join(splits_path, "flood_bolivia_data.csv")
-    train_data_path = os.path.join(splits_path, "flood_train_data.csv")
-    val_data_path = os.path.join(splits_path, "flood_valid_data.csv")
-    test_data_path = os.path.join(splits_path, "flood_test_data.csv")
 
     bolivia_data = pd.read_csv(bolivia_data_path, header=None, names=["scene", "mask"])
-    train_data = pd.read_csv(train_data_path, header=None, names=["scene", "mask"])
-    valid_data = pd.read_csv(val_data_path, header=None, names=["scene", "mask"])
-    test_data = pd.read_csv(test_data_path, header=None, names=["scene", "mask"])
 
     write_tfrecord(
-        "bolivia_dataset.tfrecord", bolivia_data, S2Hand_path, LabelHand_path
+        "bolivia_data.tfrecord",
+        split_df=bolivia_data,
+        images_path=S2Hand_path,
+        labels_path=LabelHand_path,
     )
-    write_tfrecord("train_dataset.tfrecord", train_data, S2Hand_path, LabelHand_path)
-    write_tfrecord("valid_dataset.tfrecord", valid_data, S2Hand_path, LabelHand_path)
-    write_tfrecord("test_dataset.tfrecord", test_data, S2Hand_path, LabelHand_path)
+
+    raw_dataset = tf.data.TFRecordDataset("bolivia_data.tfrecord")
+
+    feature_description = get_feature_description()
+
+    parsed_dataset = raw_dataset.map(
+        lambda example_proto: _parse_function(example_proto, feature_description)
+    )
+
+    for parsed_record in parsed_dataset.take(1):
+        image_bytes = parsed_record["image"].numpy()
+        label_bytes = parsed_record["label"].numpy()
+
+        image = decode_image(image_bytes)
+        label = decode_label(label_bytes)
+
+        plt.figure()
+        plt.imshow(image[5, :, :], cmap="turbo")
+
+        plt.figure()
+        plt.imshow(label, cmap="turbo")
+
+        plt.show()
