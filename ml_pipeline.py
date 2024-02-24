3d87863adedd907d4cd17c0df9ffbdcf4d30f927
Nathan <natannvw@gmail.com>
2024-02-24 13:26:47 +0200

    calc_ndwi_df() feature_engineering()

--- ml_pipeline.py
+++ ml_pipeline.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 import tfrecord_utils
 import utils
+from model_training import train_optimize  # noqa: F401
+from itertools import chain, combinations
 
 
 def get_dataset(
@@ -50,6 +52,50 @@ def convert_to_df(images, labels):
     return df
 
 
+def calc_ndwi_df(df: pd.DataFrame, satellite: str = "sentinel2") -> pd.DataFrame:
+    """
+    Calculate the Normalized Difference Water Index (NDWI)
+    For more information see:
+    https://custom-scripts.sentinel-hub.com/custom-scripts/sentinel-2/ndwi/
+    https://en.wikipedia.org/wiki/Normalized_difference_water_index
+
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The dataframe of the scene
+    satellite : str
+        The satellite name
+
+    Returns
+    -------
+    ndvi : pandas.DataFrame
+        The NDWI values column
+
+    Example
+    -------
+    >>> import pandas as pd
+    >>> df = pd.read_csv('test.csv')
+    >>> ndvi = calc_ndvi_df(df, 'sentinel2')
+    """
+
+    if "sentinel" in satellite:
+        Green_band_number = "B03"
+        NIR_band_number = "B08"
+    else:
+        raise ValueError(f"Satellite {satellite} not implemented yet")
+
+    ndwi = (df[Green_band_number] - df[NIR_band_number]) / (
+        df[Green_band_number] + df[NIR_band_number]
+    )
+
+    return ndwi
+
+
+def feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
+    df["NDWI"] = calc_ndwi_df(df, satellite="sentinel2")
+
+    return df
 def ml_pipeline(
     target: str = "water",
     experiment_name: str = "SenFloods",
@@ -62,6 +108,9 @@ def ml_pipeline(
     train_df = convert_to_df(train_images, train_labels)
     valid_df = convert_to_df(valid_images, val_labels)
     model = RandomForestClassifier()
+
+    train_df = feature_engineering(train_df)
+    valid_df = feature_engineering(valid_df)
 
 
 if __name__ == "__main__":
